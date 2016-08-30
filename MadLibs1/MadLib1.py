1cb38710635cfa2e52440b5a7ee0d69f3df8ca10
eestey <eestey@students.mchenry.edu>
2016-08-30 12:25:21 -0500

    Add files via upload

---
+++ MadLibs1/MadLib1.py
@@ -0,0 +1,16 @@
+# Variables
+animal = "squirrel"
+direction = "around"
+object =  "tree"
+example = "snow"
+action = "trapped"
+place = " inside"
+action2 = "melted"
+
+# poem
+print("The itsy bitsy " + animal + " climbed " + direction + " the " + object + ".")
+print("Down came the " + example)
+print("and " + action + " the " + animal + place + ".")
+print("Out came the sun")
+print("and " + action2 + " all the " + example)
+print("and the itsy bitsy " + animal + " climbed " + direction + " the " + object + " again" + ".")
